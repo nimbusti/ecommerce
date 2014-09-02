3e59f54f341eaad14a00cb76c8ce56042780d54d
hlbog <hlbog@localhost>
2014-09-02 16:05:49 -0300

    Mais itens na pagina de detalhes do produto. Criado filtro para currency.

---
+++ app/scripts/filters/filter-currency.js
@@ -0,0 +1,5 @@
+PolymerExpressions.prototype.currency = function (val, n, x, s, c) {
+	var re = '\\d(?=(\\d{' + (x || 3) + '})+' + (n > 0 ? '\\D' : '$') + ')';
+	var num = val.toFixed(Math.max(0, ~~n));
+	return (c ? num.replace('.', c) : num).replace(new RegExp(re, 'g'), '$&' + (s || ','));
+};
